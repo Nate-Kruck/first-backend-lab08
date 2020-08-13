e16b85b70e2194add5e5cf6c870d2408af381434
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-12 23:01:05 -0700

    added backend

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,13 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE games (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    genre INTEGER NOT NULL,
+                    price DECIMAL NOT NULL,
+                    rating DECIMAL NOT NULL,
+                    mature BOOLEAN NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
